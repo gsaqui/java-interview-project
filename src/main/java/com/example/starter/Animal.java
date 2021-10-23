23ed7aa1be82816c5b29574e13bbd13b5537254c
gsaqui <glenn.saqui@gmail.com>
2021-10-23 07:41:34 -0700

    Initial commit

---
+++ src/main/java/com/example/starter/Animal.java
@@ -0,0 +1,50 @@
+package com.example.starter;
+
+
+public class Animal {
+
+
+    private int id;
+    private double price;
+    private String type;
+    private String name;
+
+    public Animal(int id, double price, String type, String name) {
+        this.id = id;
+        this.price = price;
+        this.type = type;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -0,0 +1,57 @@
+package com.example.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        HttpServer server = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+
+        router.get("/")
+                // this handler will ensure that the Pojo is serialized to json
+                // the content type is set to "application/json"
+                .respond(
+                        ctx -> {
+                            String animalType = ctx.queryParams().get("type");
+                            Integer numOfAnimals = Integer.parseInt(ctx.queryParams().get("numOfAnimals"));
+
+                            return Future.succeededFuture(this.data());
+                        });
+
+        router.post("/")
+                // this handler will ensure that the Pojo is serialized to json
+                // the content type is set to "application/json"
+                .respond(
+                        ctx -> {
+                            String data = ctx.getBodyAsString();
+                            return Future.succeededFuture(data);
+                        });
+
+        server.requestHandler(router).listen(8888);
+    }
+
+    private List<Animal> data() {
+        List<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Animal(1, 5.0, "dog", "bulldog"));
+        animals.add(new Animal(2, 10, "dog", "poodle"));
+        animals.add(new Animal(3, 15, "dog", "beagle"));
+        animals.add(new Animal(4, 5, "cat", "persian"));
+        animals.add(new Animal(5, 100, "cat", "russian blue"));
+        animals.add(new Animal(6, 15, "cat", "scottish fold"));
+        animals.add(new Animal(7, 15, "cat", "siamese"));
+        animals.add(new Animal(8, 100, "cow", "angus"));
+        animals.add(new Animal(9, 200, "cow", "hereford"));
+        animals.add(new Animal(10, 1000, "cow", "guernsey"));
+        return animals;
+    }
+}
